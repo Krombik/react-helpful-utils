3858df42100aed2aa1435321bb4dca7b41a6b5a3
Andrii <jitelgor@gmail.com>
2024-10-19 12:06:39 +0300

    1.0.6

--- src/useForceRerender/index.ts
+++ src/useForceRerender/index.ts
@@ -1,12 +1,11 @@
 import { useReducer } from 'react';
 
-const toggleReducer = (state: boolean) => !state;
+const reducer = () => ({});
 
 /**
  * React hook to force a component rerender.
  * @returns A function that triggers a rerender of the component.
  */
-const useForceRerender: () => () => void = () =>
-  useReducer(toggleReducer, false)[1];
+const useForceRerender: () => () => void = () => useReducer(reducer, null!)[1];
 
 export default useForceRerender;
